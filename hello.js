1650c02bd332f90234f96755314d10743ea02d50
abdullah-shiwani <abdullahshiwani@gmail.com>
2015-03-01 14:53:25 +0500

    Delete hello.js

--- hello.js
+++
@@ -1,4 +0,0 @@
-14:34  fWaverages: 1.20 1.32 1.40
-USER     TTY      FROM              LOGIN@  IDLE WHAT
-abdullahshiwani console  -                13:04    1:29 -
-abdullahshiwani s000     -                13:48       - w
